fb1b193cbae67c6120754c5e4c30e4821a7f952d
phpsantos <phr.psantos@hotmail.com>
2024-11-30 13:09:11 -0300

    testes com bordas

--- frontend-facetopay/src/components/HeaderHeroPage.js
+++ frontend-facetopay/src/components/HeaderHeroPage.js
@@ -8,7 +8,7 @@ const HeaderHeroPage = ({navigation, mobileMenuOpen, setMobileMenuOpen}) => {
     return (
         <header className="absolute inset-x-0 top-0 z-50">
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
-                    <div className="flex lg:flex-1">
+                    <div style={{border: '3px solid'}} className="flex lg:flex-1">
                         <a href="/your-company-corrigir" className="-m-1.5 p-1.5">
                             <span className="sr-only">Facetopay</span>
                             <img
@@ -18,7 +18,7 @@ const HeaderHeroPage = ({navigation, mobileMenuOpen, setMobileMenuOpen}) => {
                             />
                         </a>
                     </div>
-                    <div className="flex lg:hidden">
+                    <div style={{border: '3px solid'}}  className="flex lg:hidden">
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(true)}
@@ -35,14 +35,14 @@ const HeaderHeroPage = ({navigation, mobileMenuOpen, setMobileMenuOpen}) => {
                             </a>
                         ))}
                     </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <div style={{border: '3px solid'}}  className="hidden lg:flex lg:flex-1 lg:justify-end">
                         <a href="/entrar-agora-corrigir" className="text-sm/6 font-semibold text-gray-900">
                             Entre Agora <span aria-hidden="true">&rarr;</span>
                         </a>
                     </div>
                 </nav>
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                    <div className="fixed inset-0 z-50" />
+                    <div style={{border: '3px solid'}}  className="fixed inset-0 z-50" />
                     <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
                             <a href="/your-company-corrigir" className="-m-1.5 p-1.5">
